be303b5fa3ccba99858cdb8451ea437fcbbd83b5
datalore92 <benh95482@gmail.com>
2025-04-14 17:51:28 -0700

    Fix _setjmp signature in setjmp_alias.c for MinGW compatibility

--- src/setjmp_alias.c
+++ src/setjmp_alias.c
@@ -1,3 +1,3 @@
 #include <setjmp.h>
-int _setjmp(jmp_buf env) { return setjmp(env); }
+int _setjmp(jmp_buf env, void* ctx) { return setjmp(env); }
 void _longjmp(jmp_buf env, int val) { longjmp(env, val); }
